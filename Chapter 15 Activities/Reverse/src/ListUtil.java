fb499f670a5d70d27427c3dd4293f68f2baeb463
Anay Apte <109878765+AnayApte@users.noreply.github.com>
2023-09-18 13:59:16 -0500

    finished the program

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -12,6 +12,20 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+        /*
+        ListIterator<String> iterator = strings.listIterator(strings.size()-1);
+        for(int i = 0; i < strings.size()-1; i++)
+        {
+            String n = iterator.previous();
+            iterator.remove();
+            strings.addLast(n);
+        }
+        */
+        for(int i = 0; i < strings.size()/2; i++)
+        {
+            String temp = strings.get(i);
+            strings.set(i, strings.get(strings.size()-1-i));
+            strings.set(strings.size()-1-i,temp);
+        }
     }
 }
